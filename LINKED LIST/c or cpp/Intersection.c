52ccc9bd94a893f3f6eed6c346464701bf062950
taniyask <67826150+taniyask@users.noreply.github.com>
2020-10-11 16:54:38 +0530

    C code to implement sets
    This is a C code that implements the mathematical concept of sets using doubly linked list.

---
+++ LINKED LIST/c or cpp/Intersection.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dll.h"
+//Author: Taniya Kulkarni
+
+struct node *list3 = NULL;
+
+void intersection (struct node *head1, struct node *head2)
+{
+	isempty(head1);
+	isempty(head2);
+	if( head1 != NULL && head2 != NULL)
+	{
+		struct node *temp1 = head1;
+		while (temp1 != NULL)
+		{
+			struct node *temp2 = head2;
+			while (temp2 != NULL)
+			{
+				if(temp2->num == temp1->num)
+				{
+					insert_end (temp1->num, &list3);					
+				}
+				temp2 = temp2->next;
+			}
+			temp1 = temp1->next;
+		}
+		duplicates (&list3);
+	}
+}
+
+void main()
+{
+    int choice1, choice2;
+    struct node *head1 = NULL;
+    struct node *head2 = NULL;
+    printf ("\nLIST 1:");
+    printf("\n --------------------------\n");
+    do
+	{
+        int item;
+        printf("\n ENTER NUMBER INTO LIST 1: ");
+        scanf("%d", &item);
+        insert_end (item, &head1);
+        printf("\n DO YOU WISH TO ENTER ANOTHER NUMBER INTO THE LIST? (Any number/0): ");
+        scanf("%d", &choice1);
+    }while(choice1);
+    printf ("\nLIST 2:");
+    printf("\n --------------------------\n");
+    do
+	{
+        int item;
+        printf("\n ENTER NUMBER INTO LIST 2: ");
+        scanf("%d", &item);
+        insert_end (item, &head2);
+        printf("\n DO YOU WISH TO ENTER ANOTHER NUMBER INTO THE LIST? (Any number/0): ");
+        scanf("%d", &choice2);
+    }while(choice2);
+    printf("\n----------------------------");
+    printf("\nLIST 1: \n");
+    display(head1);
+    printf("\n----------------------------");
+    printf("\n----------------------------");
+    printf("\nLIST 2: \n");
+    display(head2);
+    printf("\n----------------------------");
+    printf("\n----------------------------");
+    printf("\n LIST 3: \n");
+    intersection(head1, head2);
+    display(list3);
+    printf("\n----------------------------\n");
+}
