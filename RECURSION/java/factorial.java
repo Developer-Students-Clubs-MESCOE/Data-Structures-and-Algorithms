a1050e5bbbf4f496496daf52a3b4765b57b337fc
Zulfa210 <57457862+Zulfa210@users.noreply.github.com>
2020-10-21 20:55:50 +0530

    Created Factorial.java

--- RECURSION/java/factorial.java
+++ RECURSION/java/factorial.java
@@ -2,7 +2,7 @@
     
     class Factorial{  
      static int factor(int n){    
-      if (n == 0)    
+      if (n == 0 || n == 1)    
         return 1;    
       else    
         return(n * factor(n-1));    
